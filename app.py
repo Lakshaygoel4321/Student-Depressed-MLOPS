82dd3dc4ccc08104859779527cbfbd40cf1ba1b1
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-04-30 13:30:33 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def predict():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
